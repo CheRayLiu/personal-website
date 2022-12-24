1aee1836ac2063ad626ebddd18b80c93946547e3
Ray Liu <rayliu109@gmail.com>
2022-12-24 03:02:19 -0500

    update website URL

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter');
 async function generate() {
   const feed = new RSS({
     title: `Ray Liu's personal website`,
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    site_url: 'https://rayliu.me',
+    feed_url: 'https://rayliu.me/feed.xml',
   });
 
   const posts = await fs.readdir(
